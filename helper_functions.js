da12ec6db67dc9d7cd4d1fccb1d2ef803b32cc6c
Marcel Hauser <marcelhauser1995@gmail.com>
2016-07-22 11:58:15 +0200

    file helper_functions.js added

--- helper_functions.js
+++ helper_functions.js
@@ -4,6 +4,7 @@
  This function uses current rotation and pace of an object to calculate how far it has to move on X and Z in one iteration (pace).
  It uses quadrants to calculate if the change of position is negative and positive and weather sin or cos hast to be used.
 
+
  */
 
 function calcMovement (angle, pace){
