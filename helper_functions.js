785068c1b0dbd8a362caf39ddb4ee8fc588f4d24
Marcel Hauser <marcelhauser1995@gmail.com>
2016-07-22 13:03:33 +0200

    file helper_functions.js added now returns an object with two attributes

--- helper_functions.js
+++ helper_functions.js
@@ -4,7 +4,6 @@
  This function uses current rotation and pace of an object to calculate how far it has to move on X and Z in one iteration (pace).
  It uses quadrants to calculate if the change of position is negative and positive and weather sin or cos hast to be used.
 
-
  */
 
 function calcMovement (angle, pace){
@@ -35,7 +34,7 @@ function calcMovement (angle, pace){
             moveX = (Math.cos(net_angle)*pace);
             break;
     }
-    var move = new Object();
+    var move = {};
     move.X = moveX;
     move.Z = moveZ;
 
