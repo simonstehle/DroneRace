f7681b028a1bbda98d73bc7526042bec1e5e7833
Marcel Hauser <marcelhauser1995@gmail.com>
2016-07-27 17:29:00 +0200

    now has a param for all four arrow directions

--- helper_functions.js
+++ helper_functions.js
@@ -86,3 +86,16 @@ function calcMovement (angle, pace, direction){
 
 
 }
+
+function acceleration(vMax, aMax, vCurr) {
+
+}
+
+function arrowUpAcc (vMax, aMax, vCurr) {
+    var m = aMax / ((0.5*vMax)*(0.5*vMax));
+    var aCurr;
+    aCurr = - m * (vCurr - (0.5*vMax))+aMax;
+    return aCurr;
+}
+
+function arrowDownAcc () {}
